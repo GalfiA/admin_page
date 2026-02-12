515d06dca04048209470ec2f45d279974aa91219
GalfiA <37382724+GalfiA@users.noreply.github.com>
2026-02-12 16:01:50 +0100

    chart size adjusted

--- components/moving-chart.tsx
+++ components/moving-chart.tsx
@@ -112,7 +112,7 @@ export function TramPriceTicker({
                 </div>
 
                 {/* Simple Sparkline Chart */}
-                <div className="h-[120px] w-full mt-2 overflow-hidden relative">
+                <div className="h-[180px] w-full mt-2 overflow-hidden relative">
                     <svg width="100%" height="100%" viewBox={`0 0 ${width} ${height}`} preserveAspectRatio="none" className="overflow-visible">
                         <defs>
                             <linearGradient id="gradientDetails" x1="0" x2="0" y1="0" y2="1">
